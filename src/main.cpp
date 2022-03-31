f0b7efcb917227712c25299d2ed11d25fd1f2b70
Tomasz Mazur <mazur.tomasz34@gmail.com>
2022-03-31 19:48:18 +0200

    Removed debug function

--- src/main.cpp
+++ src/main.cpp
@@ -9,21 +9,11 @@
 #include "regress.h"
 #include "parse.h"
 
-double fitness2(std::vector<double> &theta, Matrix<double> &X, std::vector<double> &y) {
-    std::vector<double> err = X * theta;
-    for (int i = 0; i < err.size(); ++i)
-        std::cout << "Prediction: " << err[i] << ", Actual: " << y[i] << '\n';
-    return fitness(theta, X, y);
-}
-
 int main() {
     Matrix<double> Xt, Xv, Xs;
     std::vector<double> yt, yv, ys;
 
     getData(Xt, yt, Xv, yv, Xs, ys);
-    // applyBase(Xt, [](const double & x) { return exp(-x*x/2); });
-    // applyBase(Xv, [](const double & x) { return exp(-x*x/2); });
-    // applyBase(Xs, [](const double & x) { return exp(-x*x/2); });
 
     std::cout << "RIDGE REGRESSION:\n";
     std::vector<double> theta = regress(Xt, yt, Xv, yv, ridgeRegression);
